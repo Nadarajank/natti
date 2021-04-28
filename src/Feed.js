a6f216860a1196abd86e2e802470522315603e25
Nadarajank <jobnatti@gmail.com>
2021-04-28 12:33:01 +0530

    code change

--- src/Feed.js
+++ src/Feed.js
@@ -5,11 +5,22 @@ function Feed(){
         alert ("THIS IS PAGE");
     }
     return(
-       
+          <div>
            <div className = "text">
+               <div>
                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea><br/>
                <button type="button" onClick = {()=>ntrj()} class="btn btn-success bot ">success</button>
-           </div>
+               </div>
+              <br/>
+              <br/>
+            </div >
+            <div className = "text">
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+
+            </div>
+            </div>
+
+            
 
        
     );
--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,28 @@
-
+import React ,{useState} from 'react';
 function Login(){
-    function ntrj(){
-        alert ('this is nadarajan');
-    }
+    
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+   
+    
+ const  ntrj= () => {
+    alert (email);
+    alert (password);
+ }
+   
     return(
         <div>     
               <form>
               <div class="form-group">
                   <label for="exampleInputEmail1">Email address</label>
-                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange ={(e)=>setEmail(e.target.value)}/>
                   <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div class="form-group">
                    <label for="exampleInputPassword1">Password</label>
-                     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" onChange ={(e)=>setPassword(e.target.value)}/>
                   </div>
-                  <button type="submit" class="btn btn-primary" onClick = {()=>ntrj()}>Submit</button>
+                  <button type="button" class="btn btn-primary" onClick = {()=>ntrj()}>Submit</button>
          </form>
          </div>
  
